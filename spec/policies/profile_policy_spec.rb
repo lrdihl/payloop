eece2a74aadd20cd4af46b1be54381a58c068138
Luciano Dihl <lrdihl@gmail.com>
2026-03-04 22:25:53 -0300

    refactor(admin/users): atualiza spec ProfilePolicy para customer

--- spec/policies/profile_policy_spec.rb
+++ spec/policies/profile_policy_spec.rb
@@ -3,8 +3,8 @@ require "rails_helper"
 
 RSpec.describe ProfilePolicy, type: :policy do
   let(:admin) { create(:user, :admin) }
-  let(:consumer) { create(:user, :consumer) }
-  let(:own_profile) { create(:profile, user: consumer) }
+  let(:customer) { create(:user, :customer) }
+  let(:own_profile) { create(:profile, user: customer) }
   let(:other_profile) { create(:profile) }
 
   describe "admin" do
@@ -15,15 +15,15 @@ RSpec.describe ProfilePolicy, type: :policy do
     it "NÃO pode criar diretamente" do expect(ProfilePolicy.new(admin, own_profile).create?).to be false end
   end
 
-  describe "consumer com o próprio perfil" do
-    it "NÃO pode listar perfis" do expect(ProfilePolicy.new(consumer, own_profile).index?).to be false end
-    it "pode ver o próprio perfil" do expect(ProfilePolicy.new(consumer, own_profile).show?).to be true end
-    it "pode editar o próprio perfil" do expect(ProfilePolicy.new(consumer, own_profile).update?).to be true end
-    it "NÃO pode deletar" do expect(ProfilePolicy.new(consumer, own_profile).destroy?).to be false end
+  describe "customer com o próprio perfil" do
+    it "NÃO pode listar perfis" do expect(ProfilePolicy.new(customer, own_profile).index?).to be false end
+    it "pode ver o próprio perfil" do expect(ProfilePolicy.new(customer, own_profile).show?).to be true end
+    it "pode editar o próprio perfil" do expect(ProfilePolicy.new(customer, own_profile).update?).to be true end
+    it "NÃO pode deletar" do expect(ProfilePolicy.new(customer, own_profile).destroy?).to be false end
   end
 
-  describe "consumer com perfil de outro usuário" do
-    it "NÃO pode ver" do expect(ProfilePolicy.new(consumer, other_profile).show?).to be false end
-    it "NÃO pode editar" do expect(ProfilePolicy.new(consumer, other_profile).update?).to be false end
+  describe "customer com perfil de outro usuário" do
+    it "NÃO pode ver" do expect(ProfilePolicy.new(customer, other_profile).show?).to be false end
+    it "NÃO pode editar" do expect(ProfilePolicy.new(customer, other_profile).update?).to be false end
   end
 end
