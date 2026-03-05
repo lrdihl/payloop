ef378d62c1cd57efa4d354314adcec43e0b45562
Luciano Dihl <lrdihl@gmail.com>
2026-03-04 22:19:54 -0300

    refactor(identity): renomeia trait e factory consumer para customer

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -4,14 +4,14 @@ FactoryBot.define do
     password { "senha@123" }
     password_confirmation { "senha@123" }
     confirmed_at { Time.current }
-    role { :consumer }
+    role { :customer }
 
     trait :admin do
       role { :admin }
     end
 
-    trait :consumer do
-      role { :consumer }
+    trait :customer do
+      role { :customer }
     end
 
     trait :with_profile do
@@ -21,7 +21,7 @@ FactoryBot.define do
     end
 
     # Factory completa pronta para uso nos testes de operações
-    factory :consumer_with_profile, traits: %i[consumer with_profile]
+    factory :customer_with_profile, traits: %i[customer with_profile]
     factory :admin_with_profile,    traits: %i[admin with_profile]
   end
 end
