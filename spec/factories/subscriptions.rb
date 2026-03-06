aabed18b1db4296efca04462321a49f5c32be373
Luciano Dihl <lrdihl@gmail.com>
2026-03-06 20:45:13 -0300

    feat(subscriptions): factory :subscription

--- spec/factories/subscriptions.rb
+++ spec/factories/subscriptions.rb
@@ -2,9 +2,39 @@ FactoryBot.define do
   factory :subscription do
     association :user
     association :plan
-    status       { :pending_payment }
-    joined_at    { Date.current }
+    status        { :pending_payment }
+    joined_at     { Date.current }
     next_due_date { Date.current }
-    closed_at    { Date.current >> 12 }
+    closed_at do
+      plan_instance = plan || association(:plan)
+      if plan_instance.duration_count && plan_instance.duration_type
+        Shared::Values::Period
+          .new(count: plan_instance.duration_count, type: plan_instance.duration_type)
+          .advance_from(joined_at)
+      end
+    end
+
+    trait :active do
+      status { :active }
+    end
+
+    trait :error_payment do
+      status { :error_payment }
+    end
+
+    trait :canceled do
+      status      { :canceled }
+      canceled_at { Date.current }
+    end
+
+    trait :closed do
+      status    { :closed }
+      closed_at { Date.current - 1.day }
+    end
+
+    trait :lifetime do
+      association :plan, factory: %i[plan lifetime]
+      closed_at { nil }
+    end
   end
 end
