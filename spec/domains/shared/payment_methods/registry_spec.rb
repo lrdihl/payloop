1db6d18bb7e0816687561f1947b7ed9dd2fd5162
Luciano Dihl <lrdihl@gmail.com>
2026-03-06 19:46:04 -0300

    test(payment_methods): specs de Registry com ativo/inativo

--- spec/domains/shared/payment_methods/registry_spec.rb
+++ spec/domains/shared/payment_methods/registry_spec.rb
@@ -1,11 +1,14 @@
 require "rails_helper"
 
 RSpec.describe Shared::PaymentMethods::Registry do
-  # Isola o Registry entre testes usando uma cópia limpa
   around do |example|
-    original = described_class.instance_variable_get(:@methods).dup
+    original_methods = described_class.instance_variable_get(:@methods).dup
+    original_status = described_class.instance_variable_get(:@status).dup
+    described_class.instance_variable_set(:@methods, {})
+    described_class.instance_variable_set(:@status, {})
     example.run
-    described_class.instance_variable_set(:@methods, original)
+    described_class.instance_variable_set(:@methods, original_methods)
+    described_class.instance_variable_set(:@status, original_status)
   end
 
   describe ".register e .find" do
@@ -19,6 +22,10 @@ RSpec.describe Shared::PaymentMethods::Registry do
 
     it "aceita chave como string ou symbol" do
       expect(described_class.find("dummy")).to eq(dummy_class)
+    end
+
+    it "registra como ativo por padrão" do
+      expect(described_class.active?(:dummy)).to be true
     end
   end
 
@@ -35,6 +42,43 @@ RSpec.describe Shared::PaymentMethods::Registry do
   describe ".find com chave desconhecida" do
     it "levanta KeyError" do
       expect { described_class.find(:inexistente) }.to raise_error(KeyError)
+    end
+  end
+
+  describe ".disable e .enable" do
+    let(:dummy_class) { Class.new }
+
+    before { described_class.register(:dummy, dummy_class) }
+
+    it "disable marca como inativo" do
+      described_class.disable(:dummy)
+      expect(described_class.active?(:dummy)).to be false
+    end
+
+    it "enable reativa um método desativado" do
+      described_class.disable(:dummy)
+      described_class.enable(:dummy)
+      expect(described_class.active?(:dummy)).to be true
+    end
+
+    it "find ainda resolve classe mesmo quando inativo" do
+      described_class.disable(:dummy)
+      expect(described_class.find(:dummy)).to eq(dummy_class)
+    end
+  end
+
+  describe ".active_methods" do
+    let(:dummy_a) { Class.new }
+    let(:dummy_b) { Class.new }
+
+    before do
+      described_class.register(:a, dummy_a)
+      described_class.register(:b, dummy_b)
+    end
+
+    it "retorna apenas os métodos ativos" do
+      described_class.disable(:b)
+      expect(described_class.active_methods).to eq(a: dummy_a)
     end
   end
 end
