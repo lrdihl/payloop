93185ef096cec29dcb32d8f7bbc33d80a74f7433
Luciano Dihl <lrdihl@gmail.com>
2026-03-06 20:41:23 -0300

    feat(subscriptions): associacoes User e Plan com Subscription

--- app/models/plan.rb
+++ app/models/plan.rb
@@ -31,6 +31,8 @@ class Plan < ApplicationRecord
     end
   end
 
+  has_many :subscriptions, dependent: :restrict_with_error
+
   scope :active,   -> { where(active: true) }
   scope :inactive, -> { where(active: false) }
 end
