32756b232f671b76bb6b0a7517d8df7fb47d8e15
Luciano Dihl <lrdihl@gmail.com>
2026-03-06 22:00:15 -0300

    feat(home): redirecionar customer para customer_root_path

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,8 +2,10 @@ class HomeController < ApplicationController
   skip_before_action :authenticate_user!
 
   def index
-    if user_signed_in?
+    if user_signed_in? && current_user.admin?
       redirect_to admin_users_path
+    elsif user_signed_in?
+      redirect_to customer_root_path
     else
       redirect_to new_user_session_path
     end
