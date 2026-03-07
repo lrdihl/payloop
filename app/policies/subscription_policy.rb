5ee097ee574ab6ce63a343e99ba4aed3b198a9ff
Luciano Dihl <lrdihl@gmail.com>
2026-03-07 10:42:40 -0300

    feat(subscriptions): update_payment_method? na SubscriptionPolicy

--- app/policies/subscription_policy.rb
+++ app/policies/subscription_policy.rb
@@ -1,12 +1,15 @@
 class SubscriptionPolicy < ApplicationPolicy
-  def index?    = user.admin? || user.customer?
-  def show?     = user.admin? || own?
-  def create?   = user.admin? || user.customer?
-  def cancel?   = user.admin? || own?
-  def activate? = user.admin?
-  def fail?     = user.admin?
-  def retry?    = user.admin?
-  def close?    = user.admin?
+  NON_TERMINAL_STATUSES = %w[pending_payment error_payment active].freeze
+
+  def index?                  = user.admin? || user.customer?
+  def show?                   = user.admin? || own?
+  def create?                 = user.admin? || user.customer?
+  def cancel?                 = user.admin? || own?
+  def activate?               = user.admin?
+  def fail?                   = user.admin?
+  def retry?                   = user.admin?
+  def close?                  = user.admin?
+  def update_payment_method?  = user.admin? || (own? && non_terminal?)
 
   class Scope < ApplicationPolicy::Scope
     def resolve
@@ -21,5 +24,9 @@ class SubscriptionPolicy < ApplicationPolicy
 
   def own?
     record.user == user
+  end
+
+  def non_terminal?
+    NON_TERMINAL_STATUSES.include?(record.status)
   end
 end
