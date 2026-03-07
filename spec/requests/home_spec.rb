d9238240b77617ca2a52ed2e3525fe566f32436a
Luciano Dihl <lrdihl@gmail.com>
2026-03-06 21:57:35 -0300

    test(home): spec de redirecionamento de customer para area propria

---
+++ spec/requests/home_spec.rb
@@ -0,0 +1,31 @@
+# spec/requests/home_spec.rb
+require "rails_helper"
+
+RSpec.describe "Home", type: :request do
+  describe "GET /" do
+    context "quando não autenticado" do
+      it "redireciona para login" do
+        get root_path
+        expect(response).to redirect_to(new_user_session_path)
+      end
+    end
+
+    context "quando autenticado como admin" do
+      before { sign_in create(:user, :admin) }
+
+      it "redireciona para admin_users_path" do
+        get root_path
+        expect(response).to redirect_to(admin_users_path)
+      end
+    end
+
+    context "quando autenticado como customer" do
+      before { sign_in create(:user, :customer) }
+
+      it "redireciona para customer_root_path" do
+        get root_path
+        expect(response).to redirect_to(customer_root_path)
+      end
+    end
+  end
+end
