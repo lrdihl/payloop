90c6d25140a0c515783241b55b666a1677cc8cda
Luciano Dihl <lrdihl@gmail.com>
2026-03-06 21:53:06 -0300

    feat(subscriptions): SubscriptionPolicy expandida para customer

--- app/policies/subscription_policy.rb
+++ app/policies/subscription_policy.rb
@@ -1,19 +1,25 @@
 class SubscriptionPolicy < ApplicationPolicy
-  def index?    = user.admin?
-  def show?     = user.admin?
-  def create?   = user.admin?
+  def index?    = user.admin? || user.customer?
+  def show?     = user.admin? || own?
+  def create?   = user.admin? || user.customer?
+  def cancel?   = user.admin? || own?
   def activate? = user.admin?
   def fail?     = user.admin?
   def retry?    = user.admin?
-  def cancel?   = user.admin?
   def close?    = user.admin?
 
   class Scope < ApplicationPolicy::Scope
     def resolve
-      return scope.all  if user.admin?
-      return scope.none if user.customer?
+      return scope.all            if user.admin?
+      return scope.where(user:)   if user.customer?
 
       scope.none
     end
+  end
+
+  private
+
+  def own?
+    record.user == user
   end
 end
