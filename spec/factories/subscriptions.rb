1572445cb3863285c222443e9c8e30db3fff9ef5
Luciano Dihl <lrdihl@gmail.com>
2026-03-07 10:11:13 -0300

    feat(subscriptions): trait :with_payment_method na factory de subscriptions

--- spec/factories/subscriptions.rb
+++ spec/factories/subscriptions.rb
@@ -2,9 +2,10 @@ FactoryBot.define do
   factory :subscription do
     association :user
     association :plan
-    status        { :pending_payment }
-    joined_at     { Date.current }
-    next_due_date { Date.current }
+    status          { :pending_payment }
+    payment_method  { "credit_card" }
+    joined_at       { Date.current }
+    next_due_date   { Date.current }
     closed_at do
       plan_instance = plan || association(:plan)
       if plan_instance.duration_count && plan_instance.duration_type
@@ -35,6 +36,13 @@ FactoryBot.define do
     trait :lifetime do
       association :plan, factory: %i[plan lifetime]
       closed_at { nil }
+    end
+
+    trait :with_payment_method do
+      transient do
+        method_key { "credit_card" }
+      end
+      payment_method { method_key }
     end
   end
 end
