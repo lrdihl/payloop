45d37139dd05781f058cff9a23705a2f2e0116ab
Luciano Dihl <lrdihl@gmail.com>
2026-03-07 12:32:14 -0300

    feat(billing): implementar operation ProcessPayment

---
+++ app/domains/billing/operations/process_payment.rb
@@ -0,0 +1,46 @@
+module Billing
+  module Operations
+    class ProcessPayment
+      include Dry::Transaction
+
+      step :build_payment
+      step :call_gateway
+
+      private
+
+      def build_payment(input)
+        subscription = input.fetch(:subscription)
+        plan         = subscription.plan
+
+        payment = Payment.new(
+          subscription:   subscription,
+          amount:         plan.price,
+          payment_method: subscription.payment_method,
+          status:         :pending,
+          attempt_number: input.fetch(:attempt_number)
+        )
+
+        if payment.save
+          Dry::Monads::Success(input.merge(payment: payment))
+        else
+          Dry::Monads::Failure({ type: :persistence, errors: payment.errors })
+        end
+      end
+
+      def call_gateway(input)
+        payment = input.fetch(:payment)
+        pm      = Shared::PaymentMethods::Registry.find(payment.payment_method.to_sym).new
+
+        result = pm.process(payment:)
+
+        if result.success?
+          payment.save!
+          Dry::Monads::Success(payment)
+        else
+          payment.update(status: :failed)
+          Dry::Monads::Failure({ type: :gateway, errors: result.failure })
+        end
+      end
+    end
+  end
+end
