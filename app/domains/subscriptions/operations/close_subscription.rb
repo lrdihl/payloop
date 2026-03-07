6b9ec541e48ad5002e9b36e4aa5bb7f7fc651f4e
Luciano Dihl <lrdihl@gmail.com>
2026-03-06 21:08:17 -0300

    feat(subscriptions): operation CloseSubscription

---
+++ app/domains/subscriptions/operations/close_subscription.rb
@@ -0,0 +1,28 @@
+module Subscriptions
+  module Operations
+    class CloseSubscription
+      include Dry::Transaction
+
+      step :check_transition
+      step :update_status
+
+      private
+
+      def check_transition(subscription)
+        if subscription.valid_transition?("closed")
+          Dry::Monads::Success(subscription)
+        else
+          Dry::Monads::Failure({ type: :invalid_transition, errors: { status: ["transição inválida: #{subscription.status} -> closed"] } })
+        end
+      end
+
+      def update_status(subscription)
+        if subscription.update(status: :closed)
+          Dry::Monads::Success(subscription)
+        else
+          Dry::Monads::Failure({ type: :persistence, errors: subscription.errors })
+        end
+      end
+    end
+  end
+end
